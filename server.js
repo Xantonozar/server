033ee5a37724910016dc987a1a4457c6c8ee4112
Ozar <87942063+Xantonozar@users.noreply.github.com>
2024-05-15 21:58:48 +0600

    Update server.js

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const app = express();
 const port = 3000;
 const cors = require("cors");
 app.use(cors());
-// app.get('/', (req, res) => {
-//   res.send('Hello World!')
-// })
+ app.get('/', (req, res) => {
+  res.send('Hello World!')
+ })
 const io = require("socket.io")(3000, {
   cors: {
     origin: "*",
@@ -33,6 +33,3 @@ io.on("connection", (socket) => {
     delete users[socket.id];
   });
 });
- app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
- })
