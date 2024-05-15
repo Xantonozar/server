002ba6062351e9b754fe7ecea5635688466f2673
Ozar <87942063+Xantonozar@users.noreply.github.com>
2024-05-15 22:19:45 +0600

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,33 @@
 //Node server which will handle socket  server connection
-const express = require('express');
+const express = require("express");
 const app = express();
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+const port = process.env.PORT|| 3000;
+const cors = require("cors");
+app.use(cors());
+console.log("server running");
+const io = require("socket.io")(port, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
 });
 
-app.listen(3000, () => {
-  console.log('Server running on port 3000');
+const users = {};
+io.on("connection", (socket) => {
+  console.log("user connected");
+  socket.on("new-user-joined", (name) => {
+    console.log("new user", name);
+    users[socket.id] = name;
+    socket.broadcast.emit("user-joined", name);
+  });
+  socket.on("send", (messages) => {
+    socket.broadcast.emit("recieve", {
+      messages: messages,
+      name: users[socket.id],
+    });
+  });
+  socket.on("disconnect", (messages) => {
+    socket.broadcast.emit("left", users[socket.id]);
+    delete users[socket.id];
+  });
 });
